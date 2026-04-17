c15c3e27ecbad1882a0c68b507b66d90eb9d83c8
dikie001 <omondidickens255@gmail.com>
2026-04-17 04:47:26 +0300

    fix(profile): simplify profile route by removing ProtectedRoute

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -1,12 +1,9 @@
 import { Navigate, createRoute } from "@tanstack/react-router";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import { rootRoute } from "./root";
 
 function ProfileAliasRoute() {
   return (
-    <ProtectedRoute requireRole="USER" redirectTo="/profile">
-      <Navigate to="/user/profile" />
-    </ProtectedRoute>
+    <Navigate to="/user/profile" />
   );
 }
 
